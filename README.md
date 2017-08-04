b22a5da40f0e03221e0ec61c6e622b71849b0b97
ChihYuYeh <cure100061148@gmail.com>
2017-08-04 20:00:23 +0800

    add chapter4

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Use `create-react-app` to setup a simple React app
 2. Write a Hello World React Component
 3. Display output in React with a Component's render method
+4. Set properties on React Component
--- src/App.js
+++ src/App.js
@@ -6,21 +6,35 @@ class ClassComponent extends Component {
 		// return outputs with two more pair of tags needs 
 		// parenthesis and wrap them with div parent node
 		return (
-			<h1>I am Class-based Component</h1>
+			<div>
+				<h1>I am Class-based Component</h1>
+				{this.props.text2}
+			</div>
 		);
 	}
 }
 
 // stateless function component
-const StatelessComponent = () => {
+const StatelessComponent = (props) => {
 	// return outputs with two more pair of tags needs 
 	// parenthesis and wrap them with div parent node
 	return (
 		<div>
 			<h1>I am stateless function component</h1>
-			<ClassComponent />
+			{props.txt}
+			<ClassComponent text2={props.txt}/>
+			{props.cat}
 		</div>
 	);
+}
+
+StatelessComponent.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+StatelessComponent.defaultProps = {
+	cat: 100
 }
 
 export default StatelessComponent;
