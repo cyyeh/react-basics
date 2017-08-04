ee7bde187d683a32f9e26ab111f2384a1170f9de
ChihYuYeh <cure100061148@gmail.com>
2017-08-04 23:06:47 +0800

    update chapter9

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-	render() {
-		return <Title text="123" />
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentEvent: '---'
+		}
+		this.update = this.update.bind(this);
+	} 
+
+	update(e) {
+		this.setState({currentEvent: e.type});
 	}
-}
 
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-	text(props, propName, component) {
-		if (!(propName in props))
-			return new Error(`missing ${propName}`);
-		if (props[propName].length < 6)
-			return new Error(`${propName} was too short`);
+	render() {
+		return (
+			<div>
+				<textarea 
+					onKeyPress={this.update}
+					onCopy={this.update}
+					onCut={this.update}
+					onPaste={this.update}
+					onFocus={this.update}
+					onBlur={this.update}
+					onDoubleClick={this.update}
+					onTouchStart={this.update}
+					onTouchMove={this.update}
+					onTouchEnd={this.update}
+					cols="30"
+					rows="10"/>
+				<h1>{this.state.currentEvent}</h1>
+			</div>
+		);
 	}
 }
 
